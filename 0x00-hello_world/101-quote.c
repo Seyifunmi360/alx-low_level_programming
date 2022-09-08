67b41328df17ae7d2a93a97f70e3ccd5a069062a
Seyi <trojantech360@gmail.com>
2022-09-08 15:52:24 -0700

    My task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+nclude <unistd.h>
+
+/**
+ * *main - print
+ * *
+ * Return: Always 1 (successful)
+ * */
+int main(void)
+{
+	write(2, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
