ff0b66de5efe6ccc287c5e183268354f19125f34
Seyi <trojantech360@gmail.com>
2022-09-09 16:29:28 -0700

    my task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * * main - Entry point
+ * *
+ * *Return: 0
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+		(d 1 = '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
