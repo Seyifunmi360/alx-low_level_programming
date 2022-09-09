a23ffa125fb7063044593d2775032e279800df38
Seyi <trojantech360@gmail.com>
2022-09-09 15:33:57 -0700

    my task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * * main - program that prints d alphabet in lowercase, followed by a new line
+ * * You can only use the putchar
+ * * Return: 0
+ */
+
+int main(void)
+{
+	int lw = 'a';
+
+	while (lw <= 'z')
+	{
+		if (lw == 'e' || lw == 'q')
+		{
+			lw += 1;
+		}
+		else
+		{
+			putchar(lw);
+			lw += 1;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
