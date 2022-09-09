0510b95e4c79d65806923f112f438b0ea5327d68
Seyi <trojantech360@gmail.com>
2022-09-09 16:45:35 -0700

    my task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * * main - Entry point
@@ -9,9 +11,12 @@
 int main(void)
 {
 	int d;
+	char low;
 
 	for (d = '0'; d <= '9'; d++)
 		putchar(d);
+	for (low = 'a'; low <= 'f'; low++)
+		putchar(low);
 	putchar('\n');
 	return (0);
 }
