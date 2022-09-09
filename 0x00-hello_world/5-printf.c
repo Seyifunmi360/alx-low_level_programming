a30016a0b0271b58a44d1d3d6b4f73d8b9743240
Seyi <trojantech360@gmail.com>
2022-09-08 17:22:21 -0700

    My task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * *main - Print my style
+ * *
+ * *Return: Always 0 (successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
