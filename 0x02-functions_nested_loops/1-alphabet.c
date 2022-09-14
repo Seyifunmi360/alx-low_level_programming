390a0a16494b91de35ed488567694060db014408
Seyi <trojantech360@gmail.com>
2022-09-14 01:33:01 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#incluse "main.h"
+ 
+/**
+ *print_alphabet - prints abc in lower case
+ *
+ *Return: void
+ */
+ 
+void print_alphabet(void)
+	 
+{
+	        int sh;
+	 
+		for (sh = 'a'; sh <= 'z'; sh++)
+		{
+			        _putchar(sh);
+		}
+	_putchar('\n');
+}
