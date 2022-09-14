96e29b4c6b28f55170d9d640f676f724c4c0d896
Seyi <trojantech360@gmail.com>
2022-09-13 23:45:45 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,22 @@
 #include "main.h"
-
+ 
 /**
- * *main - Entry point
- * *
- * *Return: Always success
+ *main - the driver function print _putchar followed on a new line
+ *Return: always 0
  */
 
-int main(void)
+int main(void)
 
 {
-
-	printf("_putchar\n")
-
-		return (0);
+	        _putchar('_');
+	        _putchar('p');
+	        _putchar('u');
+	        _putchar('t');
+	        _putchar('c');
+	        _putchar('h');
+	        _putchar('a');
+	        _putchar('r');
+	        _putchar('\n');
+	 
+		        return (0);
 }
